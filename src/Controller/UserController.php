60572122c0260a852e3440208eb3c823593cbfe3
Camille <camillesenechal@protonmail.com>
2021-03-25 19:31:45 +0100

    installation de sympony

---
+++ src/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class UserController
+ * @Route("/profil")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/", name="user_profil")
+     */
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'user' => $this->getUser(),
+        ]);
+    }
+
+
+}
